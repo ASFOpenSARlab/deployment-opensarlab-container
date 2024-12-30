f725ce46f3288e6c1c82549cf3cd59dcb9ca860d
segallagher <segallagher@alaska.edu>
2024-12-30 11:58:23 -0900

    refactor: apply linting and formatting

--- images/check_ecr.py
+++ images/check_ecr.py
@@ -1,27 +1,27 @@
-
 import argparse
 import sys
 
 import boto3
 
-def main(image_name:str, aws_region:str, container_namespace:str, aws_profile):
+
+def main(image_name: str, aws_region: str, container_namespace: str, aws_profile):
 
     if not image_name.isalnum():
         raise ValueError(f"{image_name} is not pure alphanumeric.")
 
-    session = None 
+    session = None
     try:
         session = boto3.Session(region_name=aws_region, profile_name=aws_profile)
-    except:
+    except Exception as _:
         session = boto3.Session(region_name=aws_region)
 
-    ecr = session.client('ecr')
+    ecr = session.client("ecr")
 
     try:
-        repositoryName=f"{container_namespace}/{image_name}"
+        repositoryName = f"{container_namespace}/{image_name}"
         _ = ecr.describe_repositories(repositoryNames=[repositoryName])
         print(f"Repo '{repositoryName}' already exists.")
-    except ecr.exceptions.RepositoryNotFoundException as e:
+    except ecr.exceptions.RepositoryNotFoundException as _:
         print(f"Repo for {repositoryName} not found. Creating...")
         _ = ecr.create_repository(repositoryName=repositoryName)
 
@@ -29,14 +29,16 @@ def main(image_name:str, aws_region:str, container_namespace:str, aws_profile):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image_name', default=None)
-    parser.add_argument('--aws_profile', default=None)
-    parser.add_argument('--aws_region', default=None)
-    parser.add_argument('--container_namespace', default=None)
+    parser.add_argument("--image_name", default=None)
+    parser.add_argument("--aws_profile", default=None)
+    parser.add_argument("--aws_region", default=None)
+    parser.add_argument("--container_namespace", default=None)
     args = parser.parse_args()
 
     try:
-        main(args.image_name, args.aws_region, args.container_namespace, args.aws_profile)
+        main(
+            args.image_name, args.aws_region, args.container_namespace, args.aws_profile
+        )
     except Exception as e:
         print(f"An error occurred: {e}")
         sys.exit(-1)
